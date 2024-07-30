8e95d10175e06d2e5900412317b0dd5e37e771e1
Ryan Deivert <rdeivert@icloud.com>
2024-07-30 00:48:26 -0700

    treesitter config now in plugin instead of init.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,5 +3,19 @@ return {
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
+
+        treesitter.setup({
+            ensure_installed = { "c" },
+            sync_install = false,
+            ignore_install = { "" },
+            highlight = {
+                enable = true,
+                disable = { "" },
+                additional_vim_regex_highlighting = false,
+            },
+        })
+    end
 }
 
