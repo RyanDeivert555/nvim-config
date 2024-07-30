0ad1b25a6f278ff5fe3fa599d9fe189f7fb16aa4
Ryan Deivert <rdeivert@icloud.com>
2024-07-30 00:11:36 -0700

    change format for treesitter plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,7 @@
-local M = {
+return {
     "nvim-treesitter/nvim-treesitter",
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
 }
-
-return { M }
 
