392f88d6a2f4449b47bc79809a93559f78cc6282
Ryan Deivert <rdeivert@icloud.com>
2024-07-29 23:27:40 -0700

    first commit

---
+++ init.lua
@@ -0,0 +1,15 @@
+require("config.lazy")
+
+vim.o.cmdheight = 1    -- Height for the command section at the bottom
+vim.o.tabstop = 4      -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4  -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4   -- Number of spaces inserted when indenting
+vim.wo.number = true   -- Add number lines
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
