d2f44a114d17fb910107a6976f1a16bcffbba541
Ryan Deivert <rdeivert@icloud.com>
2024-07-31 13:55:24 -0700

    added autocomplete

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
         local treesitter = require("nvim-treesitter.configs")
 
         treesitter.setup({
-            ensure_installed = { "c" },
+            ensure_installed = { "c", "cpp" },
             sync_install = false,
             ignore_install = { "" },
             highlight = {
