08f53a435ac55ffdd88dc608b4e12de96c7bfb02
Ryan Deivert <rdeivert@icloud.com>
2024-09-11 16:27:58 -0700

    telescope ignores some folders

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,17 @@ return {
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
         vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+        local telescope_config = require("telescope.config")
+        telescope_config.set_defaults{
+            file_ignore_patterns = {
+                ".git",
+                "bin",
+                "temp",
+                -- specific folders
+                ".zig%-cache",
+            },
+        }
     end,
 }
 
