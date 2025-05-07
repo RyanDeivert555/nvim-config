b14853bc2f96bedaed8f649266a438bf343c7631
Ryan Deivert <radeivert@gmail.com>
2025-05-07 15:13:51 -0700

    lua_ls now can use stdlib definitions

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,9 +4,44 @@ return {
         vim.diagnostic.config{
             virtual_text = true,
         }
+
         vim.lsp.enable("lua_ls")
         vim.lsp.config("lua_ls", {
-            cmd = { "lua-language-server", "--logpath=~/.cache/lua-language-server/", }
+            on_init = function(client)
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+
+                    if path ~= vim.fn.stdpath("config") and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then
+                        return
+                    end
+                end
+
+                client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                    runtime = {
+                        version = "LuaJIT",
+                        path = {
+                            "lua/?.lua",
+                            "lua/?/init.lua",
+                        },
+                    },
+                    workspace = {
+                        checkThirdPart = false,
+                        library = {
+                            vim.env.VIMRUNTIME
+                        },
+                    },
+                })
+            end,
+            settings = {
+                Lua = {
+
+                }
+            },
+            cmd = {
+                "lua-language-server",
+                "--logpath=~/.cache/lua-language-server/",
+                "--metapath=~/.cache/lua-language-server/",
+            }
         })
         vim.lsp.enable("clangd")
         vim.lsp.enable("zls")
